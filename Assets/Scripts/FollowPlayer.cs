da666e19047169f4c1cd175cb9fb71d2c3345018
TheOctan <kanatush.tim@gmail.com>
2021-01-19 17:18:23 +0300

    follow camera

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    [Header("Properies")]
+    public float smoothTime = 0.3f;
+    public int yOffset = 0;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+	{
+        Vector3 targetPosition = player.transform.TransformPoint(new Vector3(0, yOffset, -10));
+        targetPosition = new Vector3(0, targetPosition.y, targetPosition.z);
+
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
