035ed61c4c10833c93fc7f1fadbbda09fe596aee
TheOctan <kanatush.tim@gmail.com>
2021-01-24 00:06:25 +0300

    update text color

--- Assets/Scripts/Managers/GameEventHandler.cs
+++ Assets/Scripts/Managers/GameEventHandler.cs
@@ -9,25 +9,35 @@ public class GameEventHandler : MonoBehaviour
 	public GameObject gameOverPanel;
 
 	[Header("Text")]
-	public TextMeshProUGUI currentScoreText;
+	public TextMeshProUGUI bestScoreTitleText;
 	public TextMeshProUGUI bestScoreText;
 
+	public TextMeshProUGUI currentScoreTitleText;
+	public TextMeshProUGUI currentScoreText;
+
+	private Camera mainCamera;
+
 	private int currentScore;
+	private float hueValue;
+
 	private const string BEST_SCORE = "bestScore";
+
+	private void Awake()
+	{
+		mainCamera = Camera.main;
+	}
 
 	private void Start()
 	{
 		currentScore = 0;
 		bestScoreText.text = PlayerPrefs.GetInt(BEST_SCORE, 0).ToString();
+		hueValue = Random.Range(0, 10) / 10f;
+
 		SetScore();
+		SetBackgroundColor();
 	}
 
-	private void Update()
-	{
-
-	}
-
-	public void GameOver()
+	public void OnGameOver()
 	{
 		StartCoroutine(GameOverDelay());
 	}
@@ -46,6 +56,34 @@ public class GameEventHandler : MonoBehaviour
 			bestScoreText.text = currentScore.ToString();
 		}
 		SetScore();
+	}
+
+	public void OnGetItem()
+	{
+		SetBackgroundColor();
+	}
+
+	private void SetBackgroundColor()
+	{
+		var color = Color.HSVToRGB(hueValue, 0.6f, 0.8f);
+		mainCamera.backgroundColor = color;
+
+		SetTextColor(bestScoreTitleText, color.grayscale);
+		SetTextColor(bestScoreText, color.grayscale);
+		SetTextColor(currentScoreTitleText, color.grayscale);
+		SetTextColor(currentScoreText, color.grayscale);
+
+		hueValue += 0.1f;
+		if (hueValue >= 1)
+		{
+			hueValue = 0;
+		}
+	}
+
+	private void SetTextColor(TMP_Text text, float grayScale)
+	{
+		var v = Mathf.Repeat(grayScale + 0.5f, 1f);
+		text.color = new Color(v, v, v, text.color.a);
 	}
 
 	private IEnumerator GameOverDelay()
