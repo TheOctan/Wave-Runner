ab79b3bb2edbe2373b14a53d86b5a5c4f15fb5f0
TheOctan <kanatush.tim@gmail.com>
2021-01-23 23:16:45 +0300

    random generate obstacles

---
+++ Assets/Scripts/Entities/ObstacleParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleParent : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+		StartCoroutine(CalculateDistanceToPlayer());
+    }
+
+    private IEnumerator CalculateDistanceToPlayer()
+	{
+		while (true)
+		{
+			if (player.transform.position.y - transform.position.y > 50)
+			{
+				Destroy(gameObject);
+			}
+            yield return new WaitForSeconds(1f);
+		}
+	}
+}
