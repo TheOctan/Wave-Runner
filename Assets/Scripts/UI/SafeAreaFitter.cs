eceec2b72cd5a61d35839e79dc0b3ccac06fdba8
TheOctan <kanatush.tim@gmail.com>
2021-01-25 15:56:39 +0300

    safe screen area

---
+++ Assets/Scripts/UI/SafeAreaFitter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+	private void Awake()
+	{
+		var rectTransform = GetComponent<RectTransform>();
+		var safeArea = Screen.safeArea;
+
+		var anchorMin = safeArea.position;
+		var anchorMax = safeArea.min + safeArea.size;
+
+		anchorMin.x /= Screen.width;
+		anchorMin.y /= Screen.height;
+		anchorMax.x /= Screen.width;
+		anchorMax.y /= Screen.height;
+
+		rectTransform.anchorMin = anchorMin;
+		rectTransform.anchorMax = anchorMax;
+	}
+}
